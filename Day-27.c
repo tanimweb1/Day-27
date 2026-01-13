103c487f685bf3f7f8fc754ab559b52dd559a6ed
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-13 22:43:47 +0600

    Add files via upload

---
+++ Day-27.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main(){
+
+
+// CSE Lab class Day 1
+
+
+
+
+
+//ques 2
+int l,w,area,para;
+scanf("%d%d",&l,&w);
+
+area = l*w;
+para = 2*(l+w);
+
+printf("area = %d\nparamteer = %d",area,para);
+
+
+
+
+
+
+// //ques 5
+
+// float a,b,c,si;
+// scanf("%f%f%f",&a,&b,&c);
+
+// si = (a*b*c)/100;
+// printf("Simple interest is %f",si);
+
+
+
+
+
+
+    return 0;
+}
