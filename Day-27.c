a5f29f508b0ff55ee69c25ea9803400d4fd41d5f
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-13 22:44:50 +0600

    Update Day-27.c

--- Day-27.c
+++ Day-27.c
@@ -6,30 +6,20 @@ int main(){
 // CSE Lab class Day 1
 
 
+// Ques 1 
+int a,b,sum,sub,quotient,product,reminder;
+scanf("%d%d",&a,&b);
 
-
-
-//ques 2
-int l,w,area,para;
-scanf("%d%d",&l,&w);
-
-area = l*w;
-para = 2*(l+w);
-
-printf("area = %d\nparamteer = %d",area,para);
-
-
-
-
-
-
-// //ques 5
-
-// float a,b,c,si;
-// scanf("%f%f%f",&a,&b,&c);
-
-// si = (a*b*c)/100;
-// printf("Simple interest is %f",si);
+sum = a+b;
+sub = a-b;
+quotient = a/b;
+product = a*b;
+reminder = a%b;
+printf("sum %d\n",sum);
+printf("sub %d\n",sub);
+printf("quotient %d\n",quotient);
+printf("reminder %d\n",reminder);
+printf("product %d\n",product);
 
 
 
@@ -37,4 +27,5 @@ printf("area = %d\nparamteer = %d",area,para);
 
 
     return 0;
+
 }
